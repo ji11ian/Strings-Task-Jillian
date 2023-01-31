44d2cd19bd8aa2c8b260d7224a10456bbab21243
ji11ian <123039281+ji11ian@users.noreply.github.com>
2023-01-31 23:43:20 +0800

    Create String.java

---
+++ String.java
@@ -0,0 +1,62 @@
+//1
+String myFavouriteArtist = "Adele";
+
+//2
+System.out.println(myFavouriteArtist);
+
+//3
+int myFavouriteArtistLength = myFavouriteArtist.length();
+
+//4
+System.out.println(myFavouriteArtistLength);
+
+//5
+String myFavouriteArtistUpper = myFavouriteArtist.toUpperCase();
+
+//6
+System.out.println(myFavouriteArtistUpper);
+
+//7
+String myFavouriteArtistLower = myFavouriteArtist.toLowerCase();
+
+//8
+System.out.println(myFavouriteArtistLower);
+
+//9
+String checkFor = "ift";
+
+//10
+boolean myFavouriteArtistContains = myFavouriteArtist.contains(checkFor);
+
+//11
+System.out.println(myFavouriteArtistContains);
+
+//12
+String firstName = "Jillian";
+
+//13
+String lastName = "Lam";
+
+//14
+int age = 15;
+
+//15
+String myInfo;
+
+//16
+myInfo = "First name: " + firstName + "\nLast name: " + lastName + "\nAge: " + age + "\nFavourite artist: " + myFavouriteArtist;
+
+//17
+System.out.println(myInfo);
+
+//18
+String stringToIntTest = "16";
+
+//19
+int stringToIntTestStore = Integer.parseInt(stringToIntTest);
+
+//20
+String intToStringTest = "" + stringToIntTestStore;
+
+//21
+System.out.println(intToStringTest);
